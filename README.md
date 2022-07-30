c2eceda964eff930c8466da7d5317cfa96f6f676
Edwin Villacis <evillacis@truelogic.io>
2022-07-30 12:50:48 -0500

    Tests

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Recipes-Manager
+ Will save users favorite recipes and filtering results.
+ 
+ 
+ ## Get Started
+ This is recipe management application in which multiple users login and save their best recipes with details about recipe.In this application Front-End and Back-End are developed using different frameworks.
+ - Front-End Developed Using Java Swing
+ - Back-End Developed Using Springboot Framework
+ - MySQL Database
+ 
+ ###### Prerequisites
+ - Java JDK 17
+ - IntelliJ IDEA 
+ - Apache Netbeans 13
+ - Maven Build 3.XX
 
-Will save users favorite recipes and filtering results.
+ ## Front End
+ The Front End Application is build with Java Swing Package to Interact with Rest API (SpringBoot Rest API).
+ OkHttp libaray is used to call Rest API In Swing Application.
+ 
+ ## Back End
+ The Back End Rest API is build using Springboot Framework using Hibernate , JPA , Lombok etc.
+ 
+ ## Database
+ MySQL Database is used to store the application data.
+ 
--- Recipes Manager Back-End/src/test/java/com/recipes/manager/RecipesManagerRestApiApplicationTests.java
+++ Recipes Manager Back-End/src/test/java/com/recipes/manager/RecipesManagerRestApiApplicationTests.java
@@ -1,13 +1,53 @@
 package com.recipes.manager;
 
+import com.recipes.manager.entities.Ingredient;
+import com.recipes.manager.entities.Recipe;
+import com.recipes.manager.entities.User;
+import com.recipes.manager.repositories.UserRepository;
+import com.recipes.manager.services.IngredientService;
+import com.recipes.manager.services.RecipeService;
+import com.recipes.manager.services.UserService;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
 class RecipesManagerRestApiApplicationTests {
 
+    @InjectMocks
+    private RecipeService recipeService;
+    @InjectMocks
+    private UserService userService;
+    @Mock
+    private UserRepository userRepository;
+    @Autowired
+    private IngredientService ingredientService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    void contextLoads() {
+    public void findAllRecipes_ByUser() {
+        System.out.println(userService==null?"YES":"NO");
+        User user=userService.findById(1L);
+        List<Recipe> list=new ArrayList<>();
+        list.add(new Recipe(5L,"Rice","Boil Rice","3","6",3f,"Yes",3,"Boiling",user,new ArrayList<Ingredient>()));
+        when(recipeService.getAllRecipe()).thenReturn(list);
+        List<Recipe> recipes=recipeService.getAllRecipe();
+        Assert.assertEquals(1, recipes.size());
+        verify(recipeService, times(1)).getAllRecipe();
     }
 
 }
